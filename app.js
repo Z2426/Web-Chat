ae2969e46eab6b5b2003d6845ab74bfac3a5ae7d
Your Name <20110568@student.hcmute.edu.vn>
2022-10-12 11:28:16 +0700

    update

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var express = require("express");
 var config = require("config");
-var http = require("http");
+var bodyparser = require("body-parser");
+var controller = require(__dirname + "/apps/controllers");
 
-var app = express();
+var app = express(); // express
+
+// tro? ve` folder chua file ejs
+app.set("views",__dirname + "/apps/views");
+app.set("view engine","ejs");
+
+//cấu hình static folder
+app.use("/static",express.static(__dirname + "/public"));
+
+app.use(controller); // controller
+
 //var host = config.get(server.host);
 var port = config.get("server.port");
 
-/// test run
-var server = http.createServer(function(req,res){
-    res.writeHead(200,{"Content-Type":"text/plain"});
-    res.write("Hello");
-    res.end();
-});
-
-server.listen(port,function(){
+app.listen(port,function(){
    console.log("Server is running"); 
 });
