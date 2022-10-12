9ca5d959537a4a7330db26124c218a43410a283c
Your Name <20110568@student.hcmute.edu.vn>
2022-10-12 14:25:40 +0700

    update register

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -2,8 +2,14 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/",function(req,res){
+//router.use("/user",__dirname + "controllers/user.js");
+
+router.get("/login",function(req,res){
     res.render("login");
+});
+
+router.get("/register",function(req,res){
+    res.render("register");
 });
 
 module.exports = router;
